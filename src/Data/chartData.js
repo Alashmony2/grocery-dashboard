44bd9816036f27d39b4cd5e1dad39d1967e77c28
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-07-24 02:09:26 +0300

    adding dynamic data to chart

---
+++ src/Data/chartData.js
@@ -0,0 +1,11 @@
+ const weeklySales = [
+  { name: "Sun", sales: 400 },
+  { name: "Mon", sales: 300 },
+  { name: "Tue", sales: 200 },
+  { name: "Wed", sales: 278 },
+  { name: "Thu", sales: 189 },
+  { name: "Fri", sales: 239 },
+  { name: "Sat", sales: 349 },
+];
+
+export default weeklySales;
--- src/components/Charts/SalesChart.jsx
+++ src/components/Charts/SalesChart.jsx
@@ -1,21 +1,11 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
-
-const data = [
-  { name: "Sun", sales: 400 },
-  { name: "Mon", sales: 300 },
-  { name: "Tue", sales: 200 },
-  { name: "Wed", sales: 278 },
-  { name: "Thu", sales: 189 },
-  { name: "Fri", sales: 239 },
-  { name: "Sat", sales: 349 },
-];
-
+import weeklySales from "../../Data/chartData.js";
 export default function SalesChart() {
   return (
     <div className="bg-white dark:bg-gray-800 p-6 rounded-2xl shadow-md col-span-2">
       <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-white">Sales Overview</h2>
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart data={data}>
+        <LineChart data={weeklySales}>
           <CartesianGrid strokeDasharray="3 3" stroke="#ccc" />
           <XAxis dataKey="name" stroke="#8884d8" />
           <YAxis />
